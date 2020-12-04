872995c697419f4c3b8dc4a0b2bd1ad538603453
Maximchicus <da.riel@yandex.ru>
2020-12-04 20:31:32 +0300

    pr5

---
+++ pr5_Vostorgov.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(display "\n--------Задание 2--------\n")
+(display "   Быстрая сортировка")
+(display "\n-------------------------\n")
+(define (quicksort lista)
+   (cond ((null? lista) '())
+         (true (append (quicksort (lessthan (cdr lista) (car lista))) (cons (car lista) (quicksort (greaterthan (cdr lista) (car lista))))))
+   )
+)
+
+(define (lessthan listl n)
+   (cond ((null? listl) '())
+         ((< (car listl) n)
+           (cons (car listl) (lessthan (cdr listl) n))
+         )
+         (else (lessthan (cdr listl) n))
+         )
+   )
+
+(define (greaterthan listm n)
+   (cond ((null? listm) '())
+         ((> (car listm) n)
+           (cons (car listm) (greaterthan (cdr listm) n))
+         )
+         ((eq? (car listm) n) 
+           (cons (car listm) (greaterthan (cdr listm) n))
+         )
+         (else (greaterthan (cdr listm) n))
+         )
+ )
+
+(quicksort (list 7 6 5 4 3 2 1))
